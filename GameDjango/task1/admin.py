62de324b12d135e1729671b0ca035969841b4dac
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-12-24 10:38:39 +0600

    Add files via upload

---
+++ GameDjango/task1/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields =('balance',)
