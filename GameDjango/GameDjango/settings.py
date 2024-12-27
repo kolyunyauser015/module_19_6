815637ba212efef1c1e638d325ece8e91b51df20
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-12-27 06:01:42 +0600

    Add files via upload

--- GameDjango/GameDjango/settings.py
+++ GameDjango/GameDjango/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'GameDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'mj357klM&',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        }
 }
 
 
